055c5c1b331556f3c04aa6cc36b572505a1c8ada
muhhuz-coder <k213419@nu.edu.pk>
2025-07-15 11:59:40 +0500

    add home route to serve video list using HTML response

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from venv import create
 from dotenv import load_dotenv
 from fastapi import FastAPI,Request,Form,UploadFile,File
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from storage3 import create_client
@@ -24,3 +25,8 @@ SUPABASE_ANON_KEY=os.getenv("SUPABASE_ANON_KEY", "")
 SUPABASE_BUCKET=os.getenv("SUPABASE_BUCKET", "")
 
 supabse: Client = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)    
+
+@app.get("/",response_class=HTMLResponse)
+async def home(request:Request):
+    videos=supabse.storage.from_(SUPABASE_BUCKET).list()
+    return templates.TemplateResponse("home.html", {"request": request,"videos": videos})
